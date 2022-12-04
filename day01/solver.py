0a981a1f219e2d5e0c023bccf71876ca8605ab89
Kevin <kevin@hippocmms.com>
2022-12-04 01:09:00 -0500

    Day 1 de-refactor

--- day01/solver.py
+++ day01/solver.py
@@ -3,15 +3,17 @@ from base_solver import BaseSolver
 class Day1Solver(BaseSolver):
 
     def puzzle1(self) -> str:
-        calorieTotals = self.get_elf_calories_sorted(self.get_raw_input('\n\n'))
+        calorieTotals = self.get_elf_calories(self.get_raw_input('\n\n'))
+        calorieTotals.sort(reverse=True)
         return str(calorieTotals[0])
 
     def puzzle2(self) -> str:
-        calorieTotals = self.get_elf_calories_sorted(self.get_raw_input('\n\n'))
+        calorieTotals = self.get_elf_calories(self.get_raw_input('\n\n'))
+        calorieTotals.sort(reverse=True)
         return str(sum(calorieTotals[:3]))
 
-    def get_elf_calories_sorted(self, elves: iter) -> iter:
-        return sorted([self.sum_calories(elf.split('\n')) for elf in elves], reverse=True)
+    def get_elf_calories(self, elves: iter) -> iter:
+        return [self.sum_calories(elf.split('\n')) for elf in elves]
     
     def sum_calories(self, snacks: iter) -> int:
         return sum([int(x) for x in snacks])
--- exec.py
+++ exec.py
@@ -2,7 +2,7 @@ from day01.solver import Day1Solver
 from day03.solver import Day3Solver
 
 
-solver = Day3Solver('day03/input.txt')
+solver = Day1Solver('day01/input.txt')
 
 print('puzzle 1: ' + solver.puzzle1())
 print('puzzle 2: ' + solver.puzzle2())
